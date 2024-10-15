1b3dd477806058e4dc8f2441a03ef71d2a98f575
eperotta <eperotta@proyecto404.com>
2024-10-15 14:28:20 -0300

    Add user api service on instaces

--- src/core/infrastructure/instances.ts
+++ src/core/infrastructure/instances.ts
@@ -1,10 +1,12 @@
 
 import { AuthApi } from './api/AuthApi';
+import { UserApi } from './api/UserAPi';
 import { EventApi } from './api/EventAPi';
 import { HttpClient } from './http/HttpClient';
 import { OrganizationApi } from './api/OrganizationApi';
 import { LocalStorage } from './localStorage/localStorage';
 import { AuthService } from '../domain/services/AuthService';
+import { UserService } from '../domain/services/UserService';
 import { EventService } from '../domain/services/EventService';
 import { OrganizationService } from '../domain/services/OrganizationService';
 
@@ -14,8 +16,10 @@ const httpClient = new HttpClient(session);
 const authApi = new AuthApi(httpClient);
 const organizationApi = new OrganizationApi(httpClient);
 const eventApi = new EventApi(httpClient);
+const userApi = new UserApi(httpClient);
 
 export const authService = new AuthService(authApi);
 export const organizationService = new OrganizationService(organizationApi);
 export const eventService = new EventService(eventApi);
+export const userService = new UserService(userApi);
 
