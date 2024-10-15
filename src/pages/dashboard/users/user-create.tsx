e98b66486a9f3885df77fb933998a709f1363534
Fede Cardoso <hernan.f.cardoso@gmail.com>
2024-10-15 17:17:43 -0300

    feat. add styling

--- src/pages/dashboard/users/user-create.tsx
+++ src/pages/dashboard/users/user-create.tsx
@@ -1,20 +1,28 @@
 import { Helmet } from "react-helmet-async";
 import { useTranslation } from "react-i18next";
 
+import { Card, Grid } from "@mui/material";
+
 import { capitalizeFirtsLetter } from "src/utils/helper";
 
 import { CONFIG } from "src/config-global";
+import { DashboardContent } from "src/layouts/dashboard";
 
 import { BlankView } from "src/sections/blank/view";
 
 export default function Page() {
     const { t } = useTranslation();
     const metadata = { title: `Usuarios | Nuevo Usuario - ${CONFIG.appName}` };
-
+    const cardStyle = { mt: 5, p: 3, backgroundColor: 'background.default', boxShadow: 3 };
     const renderUserCreate = (
-        <>
-         
-        </>
+        <DashboardContent maxWidth="xl"> 
+           <Grid item xs={12}>
+ 
+       
+              <Card sx={cardStyle}>nuevo</Card>
+
+         </Grid>
+        </DashboardContent>
     )
     return (
         <>
