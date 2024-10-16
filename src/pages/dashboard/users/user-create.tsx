70319cb8c5366435043280f6b44e6fdea396bcb2
Fede Cardoso <hernan.f.cardoso@gmail.com>
2024-10-16 11:05:39 -0300

    feat. add form creation

--- src/pages/dashboard/users/user-create.tsx
+++ src/pages/dashboard/users/user-create.tsx
@@ -1,35 +1,35 @@
-import { Helmet } from "react-helmet-async";
-import { useTranslation } from "react-i18next";
+import { Helmet } from 'react-helmet-async';
+import { useTranslation } from 'react-i18next';
 
-import { Card, Grid } from "@mui/material";
+import { Card, Grid } from '@mui/material';
 
-import { capitalizeFirtsLetter } from "src/utils/helper";
+import { capitalizeFirtsLetter } from 'src/utils/helper';
 
-import { CONFIG } from "src/config-global";
-import { DashboardContent } from "src/layouts/dashboard";
+import { CONFIG } from 'src/config-global';
+import { DashboardContent } from 'src/layouts/dashboard';
 
-import { BlankView } from "src/sections/blank/view";
+import { BlankView } from 'src/sections/blank/view';
+
+import { UserNewEditForm } from './user-new-edit-form';
 
 export default function Page() {
-    const { t } = useTranslation();
-    const metadata = { title: `Usuarios | Nuevo Usuario - ${CONFIG.appName}` };
-    const cardStyle = { mt: 5, p: 3, backgroundColor: 'background.default', boxShadow: 3 };
-    const renderUserCreate = (
-        <DashboardContent maxWidth="xl"> 
-           <Grid item xs={12}>
- 
-       
-              <Card sx={cardStyle}>nuevo</Card>
-
-         </Grid>
-        </DashboardContent>
-    )
-    return (
-        <>
-          <Helmet>
-            <title>{metadata.title}</title>
-          </Helmet>
-          <BlankView title={capitalizeFirtsLetter(t('users.titleCreation'))}>{renderUserCreate}</BlankView>
-        </>
-    );
+  const { t } = useTranslation();
+  const metadata = { title: `Usuarios | Nuevo Usuario - ${CONFIG.appName}` };
+  const renderUserCreate = (
+    <DashboardContent maxWidth="xl">
+      <Grid item xs={12} sx={{ mt: 5 }}>
+        <UserNewEditForm />
+      </Grid>
+    </DashboardContent>
+  );
+  return (
+    <>
+      <Helmet>
+        <title>{metadata.title}</title>
+      </Helmet>
+      <BlankView title={capitalizeFirtsLetter(t('users.titleCreation'))}>
+        {renderUserCreate}
+      </BlankView>
+    </>
+  );
 }
---
+++ src/pages/dashboard/users/user-new-edit-form.tsx
@@ -0,0 +1,113 @@
+import type { User } from 'src/core/domain/models/user';
+
+import { z as zod } from 'zod';
+import { useMemo } from 'react';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import Stack from '@mui/material/Stack';
+import Grid from '@mui/material/Unstable_Grid2';
+import LoadingButton from '@mui/lab/LoadingButton';
+import { Select, Checkbox, MenuItem, InputLabel, FormControl, ListItemText } from '@mui/material';
+
+import { Form, Field } from 'src/components/hook-form';
+
+
+// ----------------------------------------------------------------------
+
+export type NewUserSchemaType = zod.infer<typeof NewUserSchema>;
+
+export const NewUserSchema = zod.object({
+  email: zod
+    .string()
+    .min(1, { message: 'Email is required!' })
+    .email({ message: 'Email must be a valid email address!' }),
+  role: zod.array(zod.string()).min(1, { message: 'At least one role is required!' }),
+});
+
+// ----------------------------------------------------------------------
+
+type Props = {
+  currentUser?: User;
+};
+
+export function UserNewEditForm({ currentUser }: Props) {
+  const rolesOptions = ['SUPER_ADMIN', 'SO_ADMIN', 'SO_ASSISTANT'];
+
+  const defaultValues = useMemo(
+    () => ({
+      email: currentUser?.email || '',
+      role: Array.isArray(currentUser?.roles) ? currentUser.roles.filter(Boolean) : [], // Evitar valores vacíos
+    }),
+    [currentUser]
+  );
+
+  const methods = useForm<NewUserSchemaType>({
+    mode: 'onSubmit',
+    resolver: zodResolver(NewUserSchema),
+    defaultValues,
+  });
+
+  const {
+    watch,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = methods;
+
+  const values = watch();
+
+  const onSubmit = handleSubmit(async (data) => {
+    console.log(data);
+  });
+
+  const menuProps = {
+    PaperProps: {
+      sx: {
+        background: 'white', // Fondo blanco
+      },
+    },
+  };
+
+  return (
+    <Form methods={methods} onSubmit={onSubmit}>
+      <Grid container justifyContent="center">
+        <Grid xs={12} md={6}>
+          <Card sx={{ p: 3 }}>
+            <Box rowGap={3} display="grid">
+              <Field.Text name="email" label="Email address" />
+              <FormControl fullWidth>
+                <InputLabel>Role</InputLabel>
+                <Select
+                  label="Role"
+                  name="role"
+                  multiple
+                  value={values.role || []}
+                  onChange={(e) => methods.setValue('role', e.target.value as string[])}
+                  renderValue={(selected) =>
+                    Array.isArray(selected) && selected.length > 0 ? selected.join(', ') : ''
+                  }
+                  MenuProps={menuProps}
+                >
+                  {rolesOptions.map((role) => (
+                    <MenuItem key={role} value={role}>
+                      <Checkbox checked={values.role.includes(role)} />
+                      <ListItemText primary={role} />
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Box>
+
+            <Stack alignItems="flex-end" sx={{ mt: 3 }}>
+              <LoadingButton type="submit" variant="contained" loading={isSubmitting}>
+                {!currentUser ? 'Create user' : 'Save changes'}
+              </LoadingButton>
+            </Stack>
+          </Card>
+        </Grid>
+      </Grid>
+    </Form>
+  );
+}
